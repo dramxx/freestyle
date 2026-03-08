9e02759bc7f1ecfccd48c6e2162947cf0f52ee31
DMA <drahoslav.madar@gmail.com>
2026-03-08 13:45:26 +0100

    Fix base URL for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/codecollab/", // Replace with your repo name
+  base: "/freestyle/", // Your repo name
   build: {
     outDir: "docs",
   },
